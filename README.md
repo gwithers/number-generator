deb6fa2350a87721c049d4dcedcb0201243635e4
George Withers <george.withers@bluecieloecm.com>
2017-01-11 15:34:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Building a RESTful API in Node and Express
+
+Redis-based number generator in node.
+
+[From this tutorial](http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4)
+
+## Requirements
+
+- Node and npm
+
+## Installation
+
+- Clone the repo: `git clone git@github.com:gwithers/number-generator`
+- Install dependencies: `npm install`
+- Start the server: `node server.js`
+
+## Testing the API
+TODO
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'); 
+var app     = express();
+var redis   = require('redis'),
+    client  = redis.createClient();
+var router  = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({ message: 'WORKING' });	
+});
+
+router.post('/package-number/:tenant_id/:prefix', function(req, res) {
+  console.log("Asking for a package number for " + req.params.tenant_id + ":" + req.params.prefix);
+  client.incr("package-numbers:" + req.params.tenant_id + ":" + req.params.prefix, function(error, result) {
+	res.json({ tenantId: req.params.tenant_id, prefix: req.params.prefix, number: result});
+  });
+});
+
+app.use('/api', router);
+app.listen(8080);
+console.log('Listening on 8080...');
